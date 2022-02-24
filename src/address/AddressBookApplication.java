9539f4d5d5956932a97a2b62679cbeb84b6c8e93
npham22 <npham22@horizon.csueastbay.edu>
2022-02-23 19:09:55 -0800

    Deleted commented code in main method

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -31,23 +31,6 @@ public class AddressBookApplication {
         catch (IOException except) {
             System.out.println("File I/O error occurred: " + except.getMessage());
         }
-
-        // initAddressBookExercise(addressBook);
-
-        /*
-        // Create an instance of Menu
-        Menu myMenu = new Menu();
-
-        // Display all the Menu prompts to standard output
-        System.out.println(myMenu.prompt_FirstName());
-        System.out.println(myMenu.prompt_LastName());
-        System.out.println(myMenu.prompt_Street());
-        System.out.println(myMenu.prompt_City());
-        System.out.println(myMenu.prompt_State());
-        System.out.println(myMenu.prompt_Zip());
-        System.out.println(myMenu.prompt_Telephone());
-        System.out.println(myMenu.prompt_Email());
-        */
     }
 
     /**
