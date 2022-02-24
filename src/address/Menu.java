93358b9b090c1a9af6ddaab4a41b265c8896e90d
npham22 <npham22@horizon.csueastbay.edu>
2022-02-23 22:23:13 -0800

    Altered prompt methods in Menu

--- src/address/Menu.java
+++ src/address/Menu.java
@@ -1,71 +1,90 @@
 package address;
 
+import java.util.Scanner;
+
 /**
  * Menu class currently only has static methods to prompt to standard output information about a Contact like name, etc.
  */
 public class Menu {
 
     /**
+     * userInput is the object used to get the user input as Strings
+     */
+    private static final Scanner userInput = new Scanner(System.in);
+
+    /**
      * prompt_FirstName generates a standard output prompt for the First Name to be entered
      * @return The string "First Name:"
      */
-    public String prompt_FirstName() {
-        return "First Name:";
+    public static String prompt_FirstName() {
+        System.out.println("First Name: ");
+        return userInput.nextLine();
     }
 
     /**
      * prompt_LastName generates a standard output prompt for the Last Name to be entered
      * @return The string "Last Name:"
      */
-    public String prompt_LastName() {
-        return "Last Name:";
+    public static String prompt_LastName() {
+        System.out.println("Last Name: ");
+        return userInput.nextLine();
     }
 
     /**
      * prompt_Street generates a standard output prompt for the Street to be entered
      * @return The string "Street:"
      */
-    public String prompt_Street() {
-        return "Street:";
+    public static String prompt_Street() {
+        System.out.println("Street: ");
+        return userInput.nextLine();
     }
 
     /**
      * prompt_City generates a standard output prompt for the City to be entered
      * @return The string "City:"
      */
-    public String prompt_City() {
-        return "City:";
+    public static String prompt_City() {
+        System.out.println("City: ");
+        return userInput.nextLine();
     }
 
     /**
      * prompt_State generates a standard output prompt for the State to be entered
      * @return The string "State:"
      */
-    public String prompt_State() {
-        return "State:";
+    public static String prompt_State() {
+        System.out.println("State: ");
+        return userInput.nextLine();
     }
 
     /**
      * prompt_Zip generates a standard output prompt for the Zip Code to be entered
      * @return The string "Zip:"
      */
-    public String prompt_Zip() {
-        return "Zip:";
+    public static int prompt_Zip() {
+        String inZip;
+
+        System.out.println("Zip: ");
+        inZip = userInput.nextLine();
+
+        return Integer.parseInt(inZip);
     }
 
     /**
      * prompt_Telephone generates a standard output prompt for the Telephone to be entered
      * @return The string "Telephone:"
      */
-    public String prompt_Telephone() {
-        return "Telephone:";
+    public static String prompt_Telephone() {
+        System.out.println("Telephone: ");
+        return userInput.nextLine();
     }
 
     /**
      * prompt_Email generates a standard output prompt for the Email to be entered
      * @return The string "Email:"
      */
-     public String prompt_Email() {
-        return "Email:";
+    public String prompt_Email() {
+        System.out.println("Email: ");
+        return userInput.nextLine();
     }
 }
