13fcc219e7acb3e20fa3763276c59ad80f9d5315
npham22 <73088233+npham22@users.noreply.github.com>
2022-02-25 22:44:59 -0800

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"address.data","l":"AddressBook"},{"p":"address","l":"AddressBookApplication"},{"p":"address.data","l":"AddressBookTest"},{"p":"address.data","l":"AddressEntry"},{"p":"address.data","l":"AddressEntryTest"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"address","l":"Menu"},{"p":"address","l":"MenuTest"}];updateSearchResults();
